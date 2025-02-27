82f46c271582ba08b29548969fef71a781953206
kunal-vaishnav <89318333+kunal-vaishnav@users.noreply.github.com>
2025-02-27 18:03:11 +0530

    Add files via upload

---
+++ Frontend/src/http.js
@@ -0,0 +1,32 @@
+import API from "./Constant.js";
+export async function fetchavailableplaces() {
+  const response = await fetch(API + "places");
+  const resdata = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+  return resdata.places;
+}
+export async function fetchupdateplaces() {
+  const response = await fetch(API + "user-places");
+  const resdata = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+  return resdata.places;
+}
+
+export async function fetchupdateuserplaces(places) {
+  const response = await fetch(API + "user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resdata = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+  return resdata.message;
+}
