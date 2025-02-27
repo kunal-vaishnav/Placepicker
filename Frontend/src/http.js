0de6eca767c4af764842ece238e1ac1516fb5b29
kunal-vaishnav <89318333+kunal-vaishnav@users.noreply.github.com>
2025-02-27 18:05:42 +0530

    Update http.js

--- Frontend/src/http.js
+++ Frontend/src/http.js
@@ -1,4 +1,4 @@
-import API from "./Constant.js";
+import {API} from "./Constant.js";
 export async function fetchavailableplaces() {
   const response = await fetch(API + "places");
   const resdata = await response.json();
